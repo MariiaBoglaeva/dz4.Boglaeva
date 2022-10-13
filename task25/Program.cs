4004bdd9e2d5b4345d475f5abc5fd0725ee33651
maribogl <maribogl@yandex.ru>
2022-10-13 15:40:24 +0300

    tasks

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите первое число (основание степени): ");
+int numberBase = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число (показатель степени): ");
+int numberExponent = Convert.ToInt32(Console.ReadLine());
+
+if (numberExponent > 0)
+{
+    int degree = DegreeNumber(numberBase, numberExponent);
+    if (degree > 0) Console.WriteLine($"Число {numberBase} в степени {numberExponent} -> {degree}");
+    else Console.WriteLine("Переполнение типа переменной");
+}
+else Console.WriteLine("Задан некорректный показатель степени");
+
+
+int DegreeNumber(int num, int exp)
+{
+    int res = num;
+    for (int i = 1; i < exp; i++)
+    {
+        try
+        {
+            checked
+            { res = res * num; }
+        }
+        catch (Exception)
+        {
+            res = -1;
+            break;
+        }
+
+    }
+    return res;
+}
--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int sumDigit = SumDigit(number);
+Console.WriteLine($"Cумма цифр числа {number} = {sumDigit}");
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
